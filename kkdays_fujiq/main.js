36ce87c5a11ab440f3f67031da3ed08655797e98
nigelpoh <58196088+nigelpoh@users.noreply.github.com>
2023-07-01 01:11:11 +0800

    feat: Completed FujiQ Scrapper for KKDays, pending testing

---
+++ kkdays_fujiq/main.js
@@ -0,0 +1,24 @@
+async function Scrape(){
+  const puppeteer = require('puppeteer-extra')
+  
+  // add stealth plugin and use defaults (all evasion techniques)
+  const StealthPlugin = require('puppeteer-extra-plugin-stealth')
+  puppeteer.use(StealthPlugin())
+
+  // puppeteer usage as normal
+  const browser = await puppeteer.launch({ headless: true })
+  const page = await browser.newPage()
+  await page.goto('https://www.kkday.com/en-sg/product/ajax_get_packages_data?prodMid=20133&previewToken=&beginDate=2023-06-12&endDate=2024-03-31')
+  await page.waitForTimeout(5000)
+  const data = await page.evaluate(() => document.body.textContent);
+  console.log(data)
+  const min_price = data['data']['PACKAGE']['378918']['sale_price']['min_price'] == null ? Infinity : data['data']['PACKAGE']['378918']['sale_price']['min_price']
+  const max_price = data['data']['PACKAGE']['378918']['sale_price']['max_price'] == null ? Infinity : data['data']['PACKAGE']['378918']['sale_price']['max_price']
+  const official_price = data['data']['PACKAGE']['378918']['sale_price']['official_price'] == null ? Infinity : data['data']['PACKAGE']['378918']['sale_price']['official_price']
+  await browser.close()
+  return Math.min(min_price, max_price, official_price)
+}
+
+Scrape().then((best_price) => {
+  console.log("Best price from KKDays: " + best_price)
+})
