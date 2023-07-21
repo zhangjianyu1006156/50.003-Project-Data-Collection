34dec8c3031bcc7f829bf3541018dc359b5b9afd
Xiangs <1006109@mymail.sutd.edu.sg>
2023-07-22 00:53:47 +0800

    update unit test

--- mongodb_adapter/mongoAdapter.test.js
+++ mongodb_adapter/mongoAdapter.test.js
@@ -2,17 +2,28 @@ const mongoAdapter = require("./adapter");
 
 describe('MongoDB connection test', () => {
     let client; 
+    const mockData = {name: "John Doe", age: 30};
     jest.setTimeout(80000);
     
     beforeAll(async () => {
         client = await mongoAdapter.connectDB();
     })
 
-    test('Connection test', async () => {  
-        const data = {name: "John Doe", age: 30};
-        let status = await mongoAdapter.insertData(client, 'test', data);
+    afterAll (async () => {
+        await client.close();
+    })
+
+    test('MGA_CN: Connection test', async () => {  
+        let status = await mongoAdapter.insertData(client, 'test', mockData);
 
         expect(status).toBe(true);
   
+    }, 70000);
+
+    test ('MGA_D: Correct data', async() => {
+        var collection = client.db('tbap').collection('test');
+        const insertedData = await collection.findOne({name: 'John Doe'});
+
+        expect(insertedData).toEqual(mockData);
     }, 70000);
   });
