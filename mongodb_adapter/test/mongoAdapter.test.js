56b6d3456a2e40f61fedcf54da0b044c06411ac5
Xiangs <1006109@mymail.sutd.edu.sg>
2023-07-22 16:20:45 +0800

    update mongo test

--- mongodb_adapter/test/mongoAdapter.test.js
+++ mongodb_adapter/test/mongoAdapter.test.js
@@ -2,24 +2,32 @@ const mongoAdapter = require("../adapter");
 
 describe('MongoDB connection test', () => {
     let client; 
-    const mockData = {name: "John Doe", age: 30};
-    jest.setTimeout(80000);
     
     beforeAll(async () => {
         client = await mongoAdapter.connectDB();
     })
 
-    test('MGA_CN: Connection test', async () => {  
-        let status = await mongoAdapter.insertData(client, 'test', mockData);
+    afterAll(async () => {
+        client.close();
+    })
 
+    it ('MGA_CN: Connection test', async () => {  
+        let status = await mongoAdapter.insertData(client, 'connectTest', [{connect: true}]);
+
+        await client.db('tbap').collection('connectTest').drop();
         expect(status).toBe(true);
   
     }, 70000);
 
-    test ('MGA_D: Correct data', async() => {
-        var collection = client.db('tbap').collection('test');
+    it ('MGA_D: Correct data', async() => {
+        const mockData = [{_id: "user_id", name: 'John Doe'}];
+        await mongoAdapter.insertData(client, 'insertDataTest', mockData);
+
+        var collection = client.db('tbap').collection('insertDataTest');
         const insertedData = await collection.findOne({name: 'John Doe'});
 
-        expect(insertedData).toEqual(mockData);
+        await client.db('tbap').collection('insertDataTest').drop();
+        expect([insertedData]).toEqual(mockData);
+
     }, 70000);
   });
